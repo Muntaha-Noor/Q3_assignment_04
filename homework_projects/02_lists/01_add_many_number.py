c35def605f9f43f831da97a7da5c0ad4d562e6f3
Muntaha-Noor <muntahanoor648@gmail.com>
2025-03-25 15:25:15 +0500

    updated homework

---
+++ homework_projects/02_lists/01_add_many_number.py
@@ -0,0 +1,17 @@
+
+def add_many_numbers(numbers) -> int:
+
+    total_so_far = 0
+    
+    for number in numbers:
+        total_so_far += number
+        
+    return total_so_far
+
+def main():
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    sum_of_number = add_many_numbers(numbers)
+    print("The sum of the numbers is:", sum_of_number)
+
+if __name__ == "__main__":
+    main()
